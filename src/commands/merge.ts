e90e155a100018384dfc1740fe4432f3cb946780
Ben Force <bforce@dresources.com>
2021-01-19 13:51:09 -0600

    feat: add removeAuth flag

--- src/commands/merge.ts
+++ src/commands/merge.ts
@@ -1,8 +1,8 @@
 import { Command, flags } from "@oclif/command";
 import { mergeGraphQLTypes, mergeTypeDefs } from "@graphql-tools/merge";
-import { makeExecutableSchema } from '@graphql-tools/schema';
+import { makeExecutableSchema } from "@graphql-tools/schema";
 import { loadFilesSync } from "@graphql-tools/load-files";
-import { print } from "graphql"
+import { print } from "graphql";
 import * as path from "path";
 import * as fs from "fs";
 
@@ -12,6 +12,10 @@ export default class Merge extends Command {
   static flags = {
     help: flags.help({ char: `h` }),
     sort: flags.boolean({ default: true }),
+    removeAuth: flags.boolean({
+      default: false,
+      description: "Removes authorization directives from the output schema",
+    }),
     input: flags.string({
       char: `i`,
       description: `glob pattern of schema files`,
@@ -32,13 +36,21 @@ export default class Merge extends Command {
       ]);
 
       this.log(`Loaded ${schemaFiles.length} types`);
-      
-      const result = mergeTypeDefs(schemaFiles, { sort: flags.sort })
+
+      let result = print(mergeTypeDefs(schemaFiles, { sort: flags.sort }));
+
+      if (flags.removeAuth) {
+        this.log(`Removing authorization directives`);
+        result = result.replace(
+          /@aws_(iam|cognito_user_pools|api_key)\s?/g,
+          ``
+        );
+      }
 
       const outputFilename = path.resolve(process.cwd(), args.file);
       this.log(`Writing to ${outputFilename}`);
 
-      await fs.promises.writeFile(outputFilename, print(result), `utf-8`);
+      await fs.promises.writeFile(outputFilename, result, `utf-8`);
     } catch (error) {
       this.error(error);
     }
